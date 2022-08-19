b594b62e85f6a563635ae9b84183466d5f2dd1bf
ronnygomezp21 <ronny.gomezpe@ug.edu.ec>
2022-08-18 19:43:07 -0500

    modificaciones

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0">!Bienvenido/a {{ auth()->user()->name }}!</h1>
+                <h1 class="m-0">!Bienvenido/a... {{ auth()->user()->name }}!</h1>
             </div>
         </div>
     </div>
