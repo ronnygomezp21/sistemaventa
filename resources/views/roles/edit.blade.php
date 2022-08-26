9e20155fc80eabae14c079bbe738a1cc81e8f189
ronnygomezp21 <ronny.gomezpe@ug.edu.ec>
2022-08-25 19:04:21 -0500

    modificaciones

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -17,35 +17,43 @@
                     @csrf
                     @method('PUT')
                     <div class="form-group">
-                        <label for="name">Nombre rol:</label>
-                        <input type="text" name="name"
-                            class="form-control  @error('name') is-invalid @enderror"
-                            value="{{ old('name', $role->name) }}">
-                        @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fa fa-file-invoice"></i></span>
+                            </div>
+                            <input type="text" name="name" class="form-control  @error('name') is-invalid @enderror"
+                                value="{{ old('name', $role->name) }}" placeholder="Nombre del rol" required>
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
                     </div>
                     <div class="form-group">
-                        <label for="permissions">Permisos:</label>
-                        <select name="permissions[]" class="form-control @error('permissions') is-invalid @enderror select2"
-                            multiple="multiple" data-placeholder="Seleccione los permisos..." style="width: 100%;">
-                            @foreach ($permissions as $permission)
-                                <option value="{{ $permission->id }}"
-                                    {{ in_array($permission->id, old('permissions', $rolePermissions, [])) ? 'selected' : '' }}>
-                                    {{ $permission->name }}</option>
-                            @endforeach
-                        </select>
-                        @error('permissions')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fa-solid fa-list-check"></i></span>
+                            </div>
+                            <select name="permissions[]"
+                                class="form-control @error('permissions') is-invalid @enderror select2" multiple="multiple"
+                                data-placeholder="Seleccione los permisos...">
+                                @foreach ($permissions as $permission)
+                                    <option value="{{ $permission->id }}"
+                                        {{ in_array($permission->id, old('permissions', $rolePermissions, [])) ? 'selected' : '' }}>
+                                        {{ $permission->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('permissions')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
 
-                            </span>
-                        @enderror
+                                </span>
+                            @enderror
+                        </div>
                     </div>
                     <div class="form-group">
-                        <button type="submit" class="btn btn-primary">Actualizar</button>
+                        <button type="submit" class="btn btn-primary"><i class="mr-2 fa fa-save"></i>Actualizar</button>
                     </div>
                 </form>
             </div>
@@ -56,7 +64,9 @@
 @section('js')
     <script>
         $(document).ready(function() {
-            $('.select2').select2();
+            $('.select2').select2({
+                width: 'resolve',
+            });
         });
     </script>
 @stop
