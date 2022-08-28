79c5788b2620823d91ffe197f90090d52a29169b
ronnygomezp21 <ronny.gomezpe@ug.edu.ec>
2022-08-28 14:50:41 -0500

    modificaciones

--- resources/views/producto/index.blade.php
+++ resources/views/producto/index.blade.php
@@ -12,52 +12,75 @@
 
 @section('content')
     <main>
-        <div class="card">
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table style="font-size: 15px" class="table table-striped table-hover table-sm" width="100%">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th style="text-align: center;" scope="col">Nombre</th>
-                                <th style="text-align: center;" scope="col">Cantidad</th>
-                                <th style="text-align: center;" scope="col">Precio</th>
-                                <th style="text-align: center;" scope="col">Categoria</th>
-                                <th style="text-align: center;" scope="col">estado</th>
-                                @can('editar-producto', 'borrar-producto')
-                                    <th style="text-align: center;" scope="col">Acciones</th>
-                                @endcan
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($productos as $producto)
-                                <tr>
-                                    <th scope="row">{{ $producto->id }}</th>
-                                    <td style="text-align: center;"> {{ $producto->descripcion }}</td>
-                                    <td style="text-align: center;">{{ $producto->cantidad }}</td>
-                                    <td style="text-align: center;">${{ $producto->precio }}</td>
-                                    <td style="text-align: center;">{{ $producto->categorias->descripcion }}</td>
-                                    <td style="text-align: center;">{{ $producto->estado }}</td>
-                                    <td style="text-align: center;">
-                                        @can('editar-producto')
-                                            <a href="" class="btn btn-warning btn-sm"><i
-                                                    class="fa-solid fa-pen-to-square"></i></a>
-                                        @endcan
-                                        @can('borrar-producto')
-                                            <a href="" class="btn btn-danger btn-sm"><i
-                                                    class="fa-solid fa-trash-can"></i></a>
-                                        @endcan
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+        <div class="card card-primary card-outline">
+            <div class="card-header">
+                <div class="card-tools">
+                    <div class="input-group input-group-sm" style="width: 200px;">
+                        <input type="search" name="table_search" class="form-control float-right"
+                            placeholder="Busqueda por nombre">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-default">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th style="text-align: center;">Cantidad</th>
+                            <th style="text-align: center;">Precio</th>
+                            <th>Categoria</th>
+                            <th>Estado</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($productos as $producto)
+                            @php
+                                if ($producto->estado == 1) {
+                                    $estado = 'Activo';
+                                    $color = 'success';
+                                } else {
+                                    $estado = 'Inactivo';
+                                    $color = 'danger';
+                                }
+                            @endphp
+                            <tr>
+                                <td>{{ $producto->id }}</td>
+                                <td>{{ $producto->descripcion }}</td>
+                                <td style="text-align: center;">{{ $producto->cantidad }}</td>
+                                <td style="text-align: center;">${{ $producto->precio }}</td>
+                                <td>{{ $producto->categorias->descripcion }}</td>
+                                <td><span class="badge badge-{{ $color }}">{{ $estado }}</span></td>
+                                <td>
+                                    @can('editar-producto')
+                                        <a href="" class="btn btn-warning btn-sm"><i
+                                                class="fa-solid fa-pen-to-square"></i></a>&nbsp;
+                                    @endcan
+                                    @can('borrar-producto')
+                                        <form action="" method="POST" class="d-inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-danger btn-sm" type="submit"><i
+                                                    class="fa-solid fa-trash"></i></button>
+                                        </form>
+                                    @endcan
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
             <div class="card-footer">
-                <div class="float-left">
-                    {{ $productos->links() }}
-                </div>
+                {{ $productos->links() }}
+
+                <span>{{ $productos->count() }} registros | página {{ $productos->currentPage() }} de
+                    {{ $productos->lastPage() }}</span>
             </div>
         </div>
     </main>
--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -20,7 +20,7 @@
             <div class="card-header">
                 <div class="card-tools">
                     <div class="input-group input-group-sm" style="width: 150px;">
-                        <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+                        <input type="search" name="table_search" class="form-control float-right" placeholder="Search">
                         <div class="input-group-append">
                             <button type="submit" class="btn btn-default">
                                 <i class="fas fa-search"></i>
